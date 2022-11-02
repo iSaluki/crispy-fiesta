8c43649f7a7f1331c41176341818351cd7b29ac8
Seth <sethmb@protonmail.com>
2022-11-02 12:46:27 +0000

    Minor changes

--- task110-server.py
+++ task110-server.py
@@ -17,8 +17,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as stream:
         while True:
             data = conn.recv(1024)
             data = data.decode("utf-8")
-            #conn.sendall(data)
+            print(f"Incoming message: {data}")
             msg = input("Type here: ")
             conn.sendall(bytes(msg,'utf-8'))
-            print(f"Incoming message: {data}")
           
